b28cf75451fbba49a4fc7c74b40f22eb790bdab9
sherri <sherri@sherricruz.com>
2015-03-16 12:33:18 -0700

    added route for home.html

--- public/app/app.js
+++ public/app/app.js
@@ -4,34 +4,12 @@
 /**
  * Created by SHERRI on 3/14/15.
  */
-angular.module('firstApp', [])
-    .controller('mainController',function(){
-        //bind this vm to view model
-        var vm = this;
-        //define vars and objects
-        //this lets them be available to our views
-        //define a basic var
-        vm.message = 'Hey there';
-        //define a list of items
-        vm.computers = [
-            { name: 'Macbook Pro', color: 'Silver',
-                nerdness: 7},
-            { name: 'Yoga 2 Pro', color: 'Gray',
-                nerdness: 6},
-            { name: 'Chromebook', color: 'Black',
-                nerdness: 5},
-        ];
-
-//info on our form
-        vm.computerData = {};
-        vm.addComputer = function() {
-// add a computer to the list
-            vm.computers.push({
-                name: vm.computerData.name,
-                color: vm.computerData.color,
-                nerdness: vm.computerData.nerdness
-            });
-// after our computer has been added, clear the form
-            vm.computerData = {};
-        };
-    });
+//adding files  to angular app
+angular.module('userApp', [
+    'ngAnimate',
+    'app.routes',
+    'authService',
+    'mainCtrl',
+    'userCtrl',
+    'userService'
+]);
--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,3 +1,14 @@
 /**
  * Created by SHERRI on 3/15/15.
  */
+//route for home page
+angular.module('app.routes', ['ngRoute'])
+.config(function ($routeProvider, $locationProvider){
+    $routeProvider
+        .when ('/', {
+        templateUrl: 'app/views/pages/home.html'
+    });
+    //get rid of the hash in the url
+    $locationProvider.html5Mode(true);
+
+});
